5d8d23b33fb538b3954d1b4f4bbf5490ebc59af2
Abdoulaye Traore <traore_a@outlook.com>
2024-10-20 23:42:54 +0200

    feat: standardize api returs in interceptor

--- src/core/response.interceptor.ts
+++ src/core/response.interceptor.ts
@@ -7,17 +7,20 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 function removeMetadata(object: Record<string, unknown>, blacklist: string[]) {
-    return transform(object, (result, value, key) => {
-        if (typeof key === 'string' && blacklist.includes(key)) {
-            return;
-        }
+    return transform<Record<string, unknown>, Record<string, unknown>>(
+        object,
+        (result, value, key) => {
+            if (typeof key === 'string' && blacklist.includes(key)) {
+                return;
+            }
 
-        if (Array.isArray(value) || isPlainObject(value)) {
-            value = removeMetadata(value as Record<string, unknown>, blacklist);
-        }
+            if (Array.isArray(value) || isPlainObject(value)) {
+                value = removeMetadata(value as Record<string, unknown>, blacklist);
+            }
 
-        Object.assign(result as object, { [key]: value });
-    });
+            Object.assign(result as object, { [key]: value });
+        },
+    );
 }
 
 @Injectable()
@@ -26,8 +29,14 @@ export class ResponseInterceptor implements NestInterceptor {
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
         return next.handle().pipe(
-            map((result) => {
-                return removeMetadata(instanceToPlain(result), this.config.blacklistProperties);
+            map((data) => {
+                return {
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                    statusCode: context.switchToHttp().getResponse().statusCode,
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                    message: data?.message,
+                    ...removeMetadata(instanceToPlain(data), this.config.blacklistProperties),
+                };
             }),
         );
     }
