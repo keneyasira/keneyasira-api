21b053c706c5db75996047ad8ae5dcd44676d14d
Abdoulaye Traore <traore_a@outlook.com>
2024-11-17 22:27:11 +0100

    feat: send role ids in athentication token

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+import { Admin } from '../admin/models/admin.model';
+import { Collaborator } from '../collaborator/models/collaborator.model';
 import { ApplicationLoggerService } from '../core/logger/application.logger.service';
+import { Patient } from '../patient/models/patient.model';
+import { Practician } from '../practician/models/practician.model';
+import { ROLE_NAMES } from '../role/role.service';
 import { UserAttributes } from '../user/models/user.model';
 import { UserService } from '../user/user.service';
 import { UserRoleService } from '../user-role/user-role.service';
 import { PasswordLessLoginDto } from './dtos/password-less-login-magic-link.dto';
+import type { Model } from 'sequelize-typescript';
 
 @Injectable()
 export class AuthenticationService {
@@ -37,9 +43,34 @@ export class AuthenticationService {
             throw new UnauthorizedException('Invalid Credentials');
         }
 
+        const roles = await this.userRoleService.getUserRoles(user.id);
+        const infos = [];
+
+        // Add specific IDs based on roles
+        const roleModels = {
+            [ROLE_NAMES.ADMIN]: Admin,
+            [ROLE_NAMES.PATIENT]: Patient,
+            [ROLE_NAMES.PRACTICIAN]: Practician,
+            [ROLE_NAMES.COLLABORATOR]: Collaborator,
+        };
+
+        for (const role in roleModels) {
+            if (roles.includes(role)) {
+                const info = await roleModels[role as keyof typeof roleModels].findOne({
+                    attributes: ['id'  ],
+                    where: { userId: user.id },
+                });
+
+                if (info) {
+                    infos.push({ [role]: { ...info.get({ plain: true }) } });
+                }
+            }
+        }
+
         const payload = {
             sub: user.id,
-            roles: await this.userRoleService.getUserRoles(user.id),
+            roles,
+            infos,
             ...user,
         };
 
